93de6cb839f2c2b86e76ac431309b4fff20a8c8b
dmtrack <rightrack888@gmail.com>
2023-12-20 13:26:23 +0500

    feat: seo added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,18 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
 import RootLayout from './layout';
 import { ReduxProvider } from '../store/provider';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <ReduxProvider>
-            <RootLayout>
-                <Component {...pageProps} />
-            </RootLayout>
-        </ReduxProvider>
-    );
+  return (
+    <>
+      <DefaultSeo title='city_quiz' description='this is fun game!' />
+      <ReduxProvider>
+        <RootLayout>
+          <Component {...pageProps} />
+        </RootLayout>
+      </ReduxProvider>
+    </>
+  );
 }
